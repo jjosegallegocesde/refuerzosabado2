2575825903a97b08361c4ee0a802c521fb51abf0
JJ <correo@gmail.com>
2020-10-10 08:07:57 -0500

    se crea refuerzo

---
+++ agregarDatos.php
@@ -0,0 +1,21 @@
+<?php 
+
+include("BaseDatos.php");
+
+if(isset($_POST["botonEnviar"])){
+
+    $nombre=$_POST["nombre"];
+
+    $operacion= new BaseDatos();
+
+    $consulta="INSERT INTO usuarios(nombre) VALUES ('$nombre')";
+
+    $operacion->alterarBaseDatos($consulta);
+
+    
+
+}
+
+
+
+?>
